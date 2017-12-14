ee608c77d7d1772f30abb7960c56dda608a096db
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-12-14 11:45:29 -0500

    update

--- OrcMathGUI/src/guiPlayer/Sampler.java
+++ OrcMathGUI/src/guiPlayer/Sampler.java
@@ -14,6 +14,7 @@ import guiTeacher.components.*;
 import guiTeacher.interfaces.KeyedComponent;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
+import mimiPackage.HeartImage;
 
 public class Sampler extends GUIApplication {
 
@@ -119,6 +120,8 @@ public class Sampler extends GUIApplication {
 				}
 			}); 
 			viewObjects.add(cb);
+			
+			viewObjects.add(new HeartImage());
 			
 		}
 		
