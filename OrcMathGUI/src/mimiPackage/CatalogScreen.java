2dd6dbc16170e861839e940aea195a58eca34e05
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-12-20 11:02:51 -0500

    added buttons

--- OrcMathGUI/src/mimiPackage/CatalogScreen.java
+++ OrcMathGUI/src/mimiPackage/CatalogScreen.java
@@ -1,16 +1,26 @@
 package mimiPackage;
 
+import java.io.File;
 import java.util.List;
 
-import guiTeacher.components.TextField;
+import javax.swing.JFrame;
+
+import guiTeacher.components.*;
+import guiTeacher.interfaces.FileRequester;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
-public class CatalogScreen extends FullFunctionScreen {
+public class CatalogScreen extends FullFunctionScreen implements FileRequester {
 	
 	private TextField descriptionBox;
 	private TextField name;
 	private TextField price;
+	private TextArea text;
+	private Button addButton;
+	private Button saveButton;
+	private Button deleteButton;
+	private FileOpenButton openButton;
+	private PVZ catalog;
 
 	public CatalogScreen(int width, int height) {
 		super(width, height);
@@ -25,6 +35,62 @@ public class CatalogScreen extends FullFunctionScreen {
 		price = new TextField(40, 140, 200, 30, "insert price", "price");
 		viewObjects.add(price);
 		
+		text = new TextArea(40, 180, 200, 30, "this is where text goes");
+		viewObjects.add(text);
+		
+		addButton = new Button(40, 210, 200, 30, "add", new Action() {
+			
+			@Override
+			public void act() {
+				addClick();
+			}
+		});
+		viewObjects.add(addButton);
+		saveButton = new Button(40, 230, 200, 30, "save", new Action() {
+
+			@Override
+			public void act() {
+				saveClick();
+			}
+		});
+		viewObjects.add(saveButton);
+		deleteButton = new Button(40, 250, 200, 30, "delete", new Action() {
+
+			@Override
+			public void act() {
+				deleteClick();
+			}
+		});
+		viewObjects.add(deleteButton);
+		
+		openButton = new FileOpenButton(40, 280, 200, 30, null, this);
+		viewObjects.add(openButton);
+		
+		catalog = new PVZ();
+	}
+
+	protected void addClick() {
+		text.setText("add button clicked");
+	}
+	
+	protected void saveClick() {
+		text.setText("save button clicked");
+	}
+	
+	protected void deleteClick() {
+		text.setText("delete button clicked");
+	}
+
+	@Override
+	public void setFile(File f) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public JFrame getWindow() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- OrcMathGUI/src/mimiPackage/FileLoadingConsole.java
+++ OrcMathGUI/src/mimiPackage/FileLoadingConsole.java
@@ -22,137 +22,137 @@ public class FileLoadingConsole {
 
 
 
- public static void main(String[] args){
+	public static void main(String[] args){
 
- List<String> content = testFileLoading();
+		List<String> content = testFileLoading();
 
- displayContent(content);
+		displayContent(content);
 
- testSaveContent("test.csv");
-
-
+		testSaveContent("test.csv");
 
 
 
- 
-
- }
 
 
 
- private static void testSaveContent(String fileName) {
 
- try{    
-
- FileWriter fw=new FileWriter(fileName);    
-
- fw.write("This file was created programmatically.");    
-
- fw.close();    
-
- System.out.println("Success! File \""+fileName+"\" saved!");
-
- }catch(IOException e){
-
- System.out.println("An IOException was thrown. \nCheck to see that the directory where you tried to save the file actually exists.");
-
- }
-
-     
-
- }
+	}
 
 
 
- private static void displayContent(List<String> content) {
+	private static void testSaveContent(String fileName) {
 
- //print the content:
+		try{    
 
- for(String item : content){
+			FileWriter fw=new FileWriter(fileName);    
 
- System.out.println(item);
+			fw.write("This file was created programmatically.");    
 
- }
+			fw.close();    
 
- }
+			System.out.println("Success! File \""+fileName+"\" saved!");
 
+		}catch(IOException e){
 
+			System.out.println("An IOException was thrown. \nCheck to see that the directory where you tried to save the file actually exists.");
 
- private static List<String> testFileLoading() {
-
- Scanner in = new Scanner(System.in);
-
- String fileName = "";
-
- List<String> content = new ArrayList<String>();
-
- //use this boolean to control the while loop. The user should have multiple chances to enter a correct filename
-
- boolean opened = false;
-
- while(!opened){
-
- try {
-
- System.out.println("Enter a file to open");
-
- fileName = in.nextLine();
-
- FileReader fileReader = new FileReader(new File(fileName));
-
- String line = "";
-
- //a BufferedReader enables us to read teh file one line at a time
-
- BufferedReader br = new BufferedReader(fileReader);
-
- while ((line = br.readLine()) != null) {
+		}
 
 
 
- content.add(line);
+	}
 
 
 
- /*
+	private static void displayContent(List<String> content) {
 
-  * useful later:
+		//print the content:
 
-  * split only a comma that has an even number of quotes ahead of it
+		for(String item : content){
+
+			System.out.println(item);
+
+		}
+
+	}
+
+
+
+	private static List<String> testFileLoading() {
+
+		Scanner in = new Scanner(System.in);
+
+		String fileName = "";
+
+		List<String> content = new ArrayList<String>();
+
+		//use this boolean to control the while loop. The user should have multiple chances to enter a correct filename
+
+		boolean opened = false;
+
+		while(!opened){
+
+			try {
+
+				System.out.println("Enter a file to open");
+
+				fileName = in.nextLine();
+
+				FileReader fileReader = new FileReader(new File(fileName));
+
+				String line = "";
+
+				//a BufferedReader enables us to read teh file one line at a time
+
+				BufferedReader br = new BufferedReader(fileReader);
+
+				while ((line = br.readLine()) != null) {
+
+
+
+					content.add(line);
+
+
+
+					/*
+
+					 * useful later:
+
+					 * split only a comma that has an even number of quotes ahead of it
 
  String[] row = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
 
-  */
+					 */
 
 
 
 
 
- }
+				}
 
- br.close();
+				br.close();
 
- opened = true;
+				opened = true;
 
- }catch (IOException e) {
+			}catch (IOException e) {
 
- System.out.println("The file name you specified does not exist.");
+				System.out.println("The file name you specified does not exist.");
 
- }
+			}
 
- }
-
- 
+		}
 
 
 
- //close the Scanner
 
- in.close();
 
- return content;
+		//close the Scanner
 
- }
+		in.close();
+
+		return content;
+
+	}
 
 
 
