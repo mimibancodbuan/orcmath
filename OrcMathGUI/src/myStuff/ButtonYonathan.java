371805c364f60581c4b6c2443eb79d4e7395347d
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-01-08 10:49:37 -0500

    classes made

---
+++ OrcMathGUI/src/myStuff/ButtonYonathan.java
@@ -0,0 +1,38 @@
+package myStuff;
+
+import java.awt.Color;
+
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+
+public class ButtonYonathan extends Button implements ButtonInterfaceMimi {
+
+	public ButtonYonathan(int x, int y, int w, int h, String text, Action action) {
+		super(x, y, w, h, text, action);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setColor(Color color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void highlight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dim() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ OrcMathGUI/src/myStuff/MoveYonathan.java
@@ -0,0 +1,16 @@
+package myStuff;
+
+public class MoveYonathan implements MoveInterfaceMimi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ButtonInterfaceMimi getButton() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
